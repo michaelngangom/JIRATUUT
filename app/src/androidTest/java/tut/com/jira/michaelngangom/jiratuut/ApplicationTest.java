e9939d524d23844088bbad9c07e62a64821cc519
Michael Ngangom <michaelngangom@Michaels-MacBook-Pro.local>
2015-10-30 23:30:23 +0100

    Initial commit

---
+++ app/src/androidTest/java/tut/com/jira/michaelngangom/jiratuut/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tut.com.jira.michaelngangom.jiratuut;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
